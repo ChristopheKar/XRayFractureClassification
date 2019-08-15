3eca8a1707ccbadeacd39fffd080c702d85f2ff8
ChristopheKar <karam.christophe@gmail.com>
2019-08-15 05:40:32 +0300

    mura wrist scratch

--- scratch.py
+++ scratch.py
@@ -17,54 +17,53 @@ from keras.models import Model, Sequential
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout, BatchNormalization, Flatten
 from keras.optimizers import Adam
 from keras.applications.inception_v3 import InceptionV3
-from keras.applications.inception_v3 import preprocess_input as preprocess_inception
 from keras.applications.inception_resnet_v2 import InceptionResNetV2
 from keras.applications.resnet50 import ResNet50
-from keras.applications.resnet50 import preprocess_input as preprocess_resnet
 from keras.applications.nasnet import NASNetLarge, NASNetMobile
 from keras.applications.vgg16 import VGG16
-from keras.applications.vgg16 import preprocess_input as preprocess_vgg
-from keras.applications.densenet import DenseNet169, DenseNet201
-from keras.applications.densenet import preprocess_input as preprocess_dense
+from keras.applications.densenet import DenseNet121, DenseNet169, DenseNet201
 from keras_preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
-from dual_path_network import DPN92, DPN98, DPN107, DPN137
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping, ReduceLROnPlateau
+from losses import binary_focal_loss, categorical_focal_loss
+
 
 # set dataset parameters
-# CLASSES = 7
-CLASSES = 1
 WIDTH, HEIGHT = 224, 224
 BATCH_SIZE = 16
+# DATASET = 'AUB_WRIST'
+# DATASET = 'MURA_ALL'
+DATASET = 'MURA_WRIST'
 
-if 'user' in os.environ['HOME']:
-    TRAIN_DIR = '/home/user/chris/datasets/wrist_fyp/split/train'
-    VAL_DIR = '/home/user/chris/datasets/wrist_fyp/split/val'
-else:
-    # AUB WRIST
+if DATASET == 'AUB_WRIST':
     TRAIN_DIR = '/home/ubuntu/wrist/datasets/split/train'
     VAL_DIR = '/home/ubuntu/wrist/datasets/split/val'
-    # MURA WRIST
-    # TRAIN_DIR = '/home/ubuntu/wrist/datasets/MURA_wrist/train'
-    # VAL_DIR = '/home/ubuntu/wrist/datasets/MURA_wrist/valid'
+    NUM_TRAIN = 15220
+    NUM_VAL = 1904
+    CLASSES = 1
 
-# AUB FYP SPLIT
-NUM_TRAIN = 15220
-NUM_VAL = 1904
 
-# MURA
-# NUM_TRAIN = 36804
-# NUM_VAL = 3197
+if DATASET == 'MURA_ALL':
+    TRAIN_DIR = '/home/ubuntu/wrist/datasets/MURA_classification/train'
+    VAL_DIR = '/home/ubuntu/wrist/datasets/MURA_classification/valid'
+    NUM_TRAIN = 36804
+    NUM_VAL = 3197
+    CLASSES = 7
 
-# MURA WRIST
-# NUM_TRAIN = 9748
-# NUM_VAL = 679
+
+if DATASET == 'MURA_WRIST':
+    TRAIN_DIR = '/home/ubuntu/wrist/datasets/MURA_wrist/train'
+    VAL_DIR = '/home/ubuntu/wrist/datasets/MURA_wrist/valid'
+    NUM_TRAIN = 9748
+    NUM_VAL = 679
+    CLASSES = 1
+
 
 # set training parameters
-EPOCHS = 300
+EPOCHS = 200
 STEPS_PER_EPOCH = NUM_TRAIN//BATCH_SIZE
 VALIDATION_STEPS = NUM_VAL//BATCH_SIZE
 
-def dataset_generator(preprocess_func):
+def dataset_generator():
 
     # create dataset generators
     train_datagen = ImageDataGenerator(
@@ -91,21 +90,38 @@ def dataset_generator(preprocess_func):
 
 def dir_generator(train_datagen, validation_datagen):
 
-    train_generator = train_datagen.flow_from_directory(
-        TRAIN_DIR,
-        target_size = (HEIGHT, WIDTH),
-        batch_size = BATCH_SIZE,
-        class_mode = 'binary')
+    if CLASSES == 1:
 
-    validation_generator = validation_datagen.flow_from_directory(
-        VAL_DIR,
-        target_size = (HEIGHT, WIDTH),
-        batch_size = BATCH_SIZE,
-        class_mode = 'binary')
+        train_generator = train_datagen.flow_from_directory(
+            TRAIN_DIR,
+            target_size = (HEIGHT, WIDTH),
+            batch_size = BATCH_SIZE,
+            class_mode = 'binary')
+
+        validation_generator = validation_datagen.flow_from_directory(
+            VAL_DIR,
+            target_size = (HEIGHT, WIDTH),
+            batch_size = BATCH_SIZE,
+            class_mode = 'binary')
+
+    elif CLASSES > 1:
+
+        train_generator = train_datagen.flow_from_directory(
+            TRAIN_DIR,
+            target_size = (HEIGHT, WIDTH),
+            batch_size = BATCH_SIZE,
+            class_mode = 'categorical')
+
+        validation_generator = validation_datagen.flow_from_directory(
+            VAL_DIR,
+            target_size = (HEIGHT, WIDTH),
+            batch_size = BATCH_SIZE,
+            class_mode = 'categorical')
+
 
     return train_generator, validation_generator
 
-def create_fclayer(conv_base):
+def create_fclayer(conv_base, pre=False):
 
     conv_base.trainable = True
 
@@ -114,40 +130,66 @@ def create_fclayer(conv_base):
     model.add(Flatten())
     model.add(Dense(1024, activation='relu'))
     model.add(Dense(512, activation='relu'))
-    model.add(Dropout(0.4))
+    # model.add(Dropout(0.4))
     model.add(Dense(256, activation='relu'))
     model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.4))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(CLASSES, activation='softmax'))
+    # model.add(Dropout(0.4))
+    # model.add(Dense(64, activation='relu'))
+    # model.add(Dense(32, activation='relu'))
+    if CLASSES == 1:
+        model.add(Dense(CLASSES, activation='sigmoid'))
+    elif CLASSES > 1:
+        model.add(Dense(CLASSES, activation='softmax'))
 
     return model
 
 def step_decay(epoch):
-	initial_lrate = 0.001
-	drop = 0.5
-	epochs_drop = 10.0
-	lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
-	return lrate
 
-def compile_model(model, opt='adam'):
+    min_lrate = 0.00000001
+    initial_lrate = 0.001
+    drop = 0.5
+    epochs_drop = 10.0
+    lrate = initial_lrate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
+    if lrate < min_lrate:
+        lrate = min_lrate
+    return lrate
 
-    if opt == 'rmsprop':
-        model.compile(loss='binary_crossentropy',
-                      optimizer='rmsprop',
-                      metrics=['accuracy'])
-    if opt == 'adam':
-        model.compile(loss='binary_crossentropy',
-                      optimizer=Adam(lr=0.0001, decay=0.01),
-                      metrics=['accuracy'])
+def compile_model(model, loss='default', lrate=0.0001):
+
+    adam = Adam(lr=lrate,
+                beta_1=0.9,
+                beta_2=0.999,
+                epsilon=None,
+                decay=0.0,
+                amsgrad=True)
+
+    if loss == 'default' and CLASSES == 1:
+        loss_f = 'binary_crossentropy'
+    if loss == 'default' and CLASSES > 1:
+        loss_f = 'categorical_crossentropy'
+    if loss == 'focal' and CLASSES == 1:
+        loss_f = [binary_focal_loss(alpha=.25, gamma=2)]
+    if loss == 'focal' and CLASSES > 1:
+        loss_f = [categorical_focal_loss(alpha=.25, gamma=2)]
+
+    model.compile(loss=loss_f,
+                  optimizer=adam,
+                  metrics=['accuracy'])
 
     return model
 
-def fit_model(model, train_gen, val_gen, output_name, log_dir, steps='norm'):
+def fit_model(model, train_gen, val_gen, output_name, log_dir):
 
     model_file = os.path.join('models', output_name)
     log_dir = os.path.join('./logs', log_dir)
+
+    # reduce learning rate on plateau
+    reduce_lr = ReduceLROnPlateau(monitor='val_loss',
+                                  factor=0.1,
+                                  patience=10,
+                                  min_delta=0.0001,
+                                  verbose=1,
+                                  min_lr=0.0000001)
 
     # save best models
     checkpoint = ModelCheckpoint(model_file,
@@ -166,13 +208,16 @@ def fit_model(model, train_gen, val_gen, output_name, log_dir, steps='norm'):
     # set up learning rate schedule
     lrate = LearningRateScheduler(step_decay)
 
+    # Early Stopping on Plateau
+    es = EarlyStopping(monitor='val_acc', mode='max', verbose=1, patience=30)
+
     # fit model
     history = model.fit_generator(train_gen,
                                   epochs=EPOCHS,
                                   steps_per_epoch=STEPS_PER_EPOCH,
                                   validation_data=val_gen,
                                   validation_steps=VALIDATION_STEPS,
-                                  callbacks=[checkpoint, tensorboard])
+                                  callbacks=[checkpoint, tensorboard, reduce_lr, es])
 
     return history, model
 
@@ -200,21 +245,22 @@ def draw_plots(hist, logs):
 
     plt.savefig(os.path.join('./logs', logs, 'loss.png'))
 
-def run_model(backbone, output, logs, opt='adam', act='relu'):
+def run_model(backbone, output, logs, loss='default'):
 
-    # base_model = backbone(include_top=False, input_shape = (HEIGHT, WIDTH, 3), weights='imagenet')
-    base_model = backbone(input_shape = (HEIGHT, WIDTH, 3))
+    base_model = backbone(include_top=False, input_shape = (HEIGHT, WIDTH, 3), weights=None)
     model = create_fclayer(base_model)
-    train_datagen, validation_datagen = dataset_generator(preprocess_func)
-    train_generator, validation_generator = dir_generator(train_datagen, validation_datagen)
-    model = compile_model(model, opt)
 
-    hist, model = fit_model(model, train_generator, validation_generator, output, logs, 'init')
+    train_datagen, validation_datagen = dataset_generator()
+    train_generator, validation_generator = dir_generator(train_datagen, validation_datagen)
+    model = compile_model(model, loss=loss)
+    from shutil import copyfile
+    copyfile(os.path.realpath(__file__), './logs/train.py')
+    hist, model = fit_model(model, train_generator, validation_generator, output, logs)
     draw_plots(hist, logs)
 
 if __name__ == '__main__':
 
     start_time = time.time()
-    run_model(DPN137, 'dpn137_aub.h5', 'dpn137_aub')
+    run_model(DenseNet169, 'd169_mura_wrist_scratch.h5', 'd169_mura_wrist_scratch', 'default')
     end_time = time.time()
     print('Total time: {:.3f}'.format((end_time - start_time)/3600))
