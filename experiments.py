289c9cee86388fcfe46b2a7f8ae9587998a009ea
ChristopheKar <karam.christophe@gmail.com>
2019-09-07 23:32:59 +0300

    new mura experiments

--- experiments.py
+++ experiments.py
@@ -133,26 +133,47 @@ if __name__ == '__main__':
     # cls.layers = 224
     # cls.train()
 
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v4')
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v4')
+    # cls.layers = 19
+    # cls.train()
+    #
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v4')
+    # cls.layers = 224
+    # cls.train()
+    #
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v5')
+    # cls.layers = 19
+    # cls.train()
+    #
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v5')
+    # cls.layers = 224
+    # cls.train()
+    #
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v6')
+    # cls.layers = 19
+    # cls.train()
+    #
+    # cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v6')
+    # cls.layers = 224
+    # cls.train()
+
+    cls = ClassifierCNN(DenseNet169, 'MURA_FINGER', 'd169_mura_finger_224_v1')
+    cls.layers = 224
+    cls.train()
+    cls = ClassifierCNN('d169_mura_class_224.h5', 'MURA_FINGER', 'd169_mura_finger_224_19_v1')
     cls.layers = 19
     cls.train()
 
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v4')
+    cls = ClassifierCNN(DenseNet169, 'MURA_SHOULDER', 'd169_mura_shoulder_224_v1')
     cls.layers = 224
     cls.train()
-
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v5')
+    cls = ClassifierCNN('d169_mura_class_224.h5', 'MURA_SHOULDER', 'd169_mura_shoulder_224_19_v1')
     cls.layers = 19
     cls.train()
 
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v5')
+    cls = ClassifierCNN(DenseNet169, 'MURA_FINGER', 'd169_mura_finger_224_v2')
     cls.layers = 224
     cls.train()
-
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_19_v6')
+    cls = ClassifierCNN('d169_mura_class_224.h5', 'MURA_FINGER', 'd169_mura_finger_224_19_v2')
     cls.layers = 19
-    cls.train()
-
-    cls = ClassifierCNN('d169_mura_class_aug_224.h5', 'AUB_WRIST', 'd169_aub_wrist_224_224_v6')
-    cls.layers = 224
     cls.train()
