b3d6f9461b31d2ee6eb9d39150c21ab0da1df466
ChristopheKar <karam.christophe@gmail.com>
2019-09-20 03:25:02 +0300

    increased augmentations

--- Classifier.py
+++ Classifier.py
@@ -254,11 +254,11 @@ class ClassifierCNN:
 
         train_datagen = ImageDataGenerator(
             rescale=1./255,
-            rotation_range=40,
-            width_shift_range=0.2,
-            height_shift_range=0.2,
-            shear_range=0.2,
-            zoom_range=0.2,
+            rotation_range=90,
+            width_shift_range=0.4,
+            height_shift_range=0.4,
+            shear_range=0.4,
+            zoom_range=0.4,
             horizontal_flip=True,
             fill_mode='nearest')
 
