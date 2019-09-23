5a007dd6891d514c1807bfb0a8194b46c5b1ba8d
ChristopheKar <karam.christophe@gmail.com>
2019-09-23 04:15:28 +0300

    new augmented dataset

--- Classifier.py
+++ Classifier.py
@@ -190,8 +190,8 @@ class ClassifierCNN:
 
         if dataset == 'AUB_DISP2':
             dataset_base = os.path.join(self.datasets_root, 'aub_disp2')
-            self.num_train = 923
-            self.num_val = 97
+            self.num_train = 11018
+            self.num_val = 1224
             self.classes = 1
 
         if dataset == 'AUB_NEW':
